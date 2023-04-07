48d3b20173f16dcf3acd18bec852943d6af5769b
Ygor Perez de Oliveira <azaz22155@gmail.com>
2023-04-07 08:49:40 +0000

    fixed dependencies

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,20 +1,19 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   // @ts-ignore
-  ...require('@spotify/prettier-config'),
-  arrowParens: "avoid",
-    rangeStart: 0,
+  arrowParens: 'avoid',
+  rangeStart: 0,
   rangeEnd: Infinity,
   requirePragma: false,
   bracketSpacing: true,
   insertPragma: false,
   printWidth: 80,
-  proseWrap: "preserve",
+  proseWrap: 'preserve',
   endOfLine: 'auto',
-  quoteProps: "as-needed",
+  quoteProps: 'as-needed',
   semi: true,
   singleQuote: true,
   tabWidth: 2,
-  trailingComma: "all",
+  trailingComma: 'all',
   useTabs: false,
 };
